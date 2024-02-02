f51d6f1c2c7a7a068dbb9b2208b27c01d2068380
RajrupDasid <rajrupofficialid@protonmail.com>
2024-02-02 10:19:43 +0530

    initial stage commit
    some controllers hasbeen made
    
    frontend folder done
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req, res) => {
+  res.send("Signup");
+  console.log("signup");
+};
+
+export const login = (req, res) => {
+  console.log("loginuser");
+};
+
+export const logout = (req, res) => {
+  console.log("logout");
+};
