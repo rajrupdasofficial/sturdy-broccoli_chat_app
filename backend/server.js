0dce3e237c6381f8e5ce627890b2609b9c0c73e3
RajrupDasid <rajrupofficialid@protonmail.com>
2024-02-03 21:11:08 +0530

    Auth controller hasbeen filledup with proper logics
    Database connection created
    
    Message and User models created
    
    JWT controller created
    
    some changes done in server controller
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connection.js";
 
 const app = express();
-
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
+dotenv.config();
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server is running on port ${PORT}`);
+});
