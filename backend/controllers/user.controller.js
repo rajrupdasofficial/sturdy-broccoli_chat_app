ada613e4fe4037223ae91c3dd65b6752fc359735
RajrupDasid <rajrupofficialid@protonmail.com>
2024-02-05 22:06:21 +0530

    messages routed created
    users route created
    
    validation added
    
    some optimization applied
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filterdUsers = await User.find({ _id: { $ne: loggedInUserId } });
+    res.status(200).json(filterdUsers);
+  } catch (error) {
+    res.status(500).json({ error: `Internal server ${error.messages}` });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoute from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connection.js";
 import cookieParser from "cookie-parser";
 
@@ -12,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoute);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello");
