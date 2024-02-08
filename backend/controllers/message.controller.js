d20b87eddcdcdb295a6ae72629eab5af6f640f4a
RajrupDasid <rajrupofficialid@protonmail.com>
2024-02-08 12:32:21 +0530

    socket layer implemented fully
    realtime functions added
    
    some bug fixes
    
    performance imporvement added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -22,10 +23,17 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
+
     // await conversation.save();
     // await newMessage.save();
     // this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
+    // socket io implement
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+    }
+    // below method to send messages to specific client
+    io.to(receiverSocketId).emit("newMessage", newMessage);
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessages controller", error.message);
--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import userRoute from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connection.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use("/api/users", userRoute);
 //   res.send("Hello");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`server is running on port ${PORT}`);
 });
