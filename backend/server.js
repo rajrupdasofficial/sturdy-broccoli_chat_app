cbbcbd036a8d1cdca8368107b968fc0d53f3f2b6
Rajrup Das <rajrupofficialid@protonmail.com>
2024-02-08 07:38:11 +0000

    config added for deployment (#3)
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,21 @@ import userRoute from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connection.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
+import path from "path";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-dotenv.config();
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoute);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //   res.send("Hello");
